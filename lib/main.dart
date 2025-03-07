b9086f449c16f566eaceec0c8f2ed2249af6a7e3
Portfolio <labeebkhan7766@gmail.com>
2025-03-07 13:20:08 +0500

    work xilophone

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  void xylophoneSound(int note) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$note.wav'));
+  }
+
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: [
+            TextButton(
+                onPressed: () {
+                  xylophoneSound(1);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        249, 244, 0, 0) // Change text color
+                    ),
+                child: Text('Play')),
+            TextButton(
+                onPressed: () {
+                  xylophoneSound(2);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 255, 123, 0) // Change text color
+                    ),
+                child: Text('Play 2')),
+            TextButton(
+                onPressed: () {
+                  xylophoneSound(3);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 255, 247, 0) // Change text color
+                    ),
+                child: Text('Play 3')),
+            TextButton(
+                onPressed: () {
+                  xylophoneSound(4);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 11, 121, 28) // Change text color
+                    ),
+                child: Text('Play4')),
+            TextButton(
+                onPressed: () {
+                  xylophoneSound(5);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 28, 92, 40) // Change text color
+                    ),
+                child: Text('Play5')),
+            TextButton(
+                onPressed: () {
+                  xylophoneSound(6);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 34, 96, 172) // Change text color
+                    ),
+                child: Text('Play6')),
+            TextButton(
+                onPressed: () {
+                  xylophoneSound(7);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 255, 0, 136) // Change text color
+                    ),
+                child: Text('Play7')),
+          ],
+        )),
+      ),
+    );
+  }
+}
