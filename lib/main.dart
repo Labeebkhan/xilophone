9d76ff429fe7c35af1ce2125eb5b42e9f9151a90
Portfolio <labeebkhan7766@gmail.com>
2025-03-07 15:02:12 +0500

    xilophone notes shape change

--- lib/main.dart
+++ lib/main.dart
@@ -19,72 +19,92 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Center(
+            child: const Text('Xylophone App'),
+          ),
+        ),
         body: SafeArea(
             child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextButton(
-                onPressed: () {
-                  xylophoneSound(1);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        249, 244, 0, 0) // Change text color
-                    ),
-                child: Text('Play')),
-            TextButton(
-                onPressed: () {
-                  xylophoneSound(2);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 255, 123, 0) // Change text color
-                    ),
-                child: Text('Play 2')),
-            TextButton(
-                onPressed: () {
-                  xylophoneSound(3);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 255, 247, 0) // Change text color
-                    ),
-                child: Text('Play 3')),
-            TextButton(
-                onPressed: () {
-                  xylophoneSound(4);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 11, 121, 28) // Change text color
-                    ),
-                child: Text('Play4')),
-            TextButton(
-                onPressed: () {
-                  xylophoneSound(5);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 28, 92, 40) // Change text color
-                    ),
-                child: Text('Play5')),
-            TextButton(
-                onPressed: () {
-                  xylophoneSound(6);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 34, 96, 172) // Change text color
-                    ),
-                child: Text('Play6')),
-            TextButton(
-                onPressed: () {
-                  xylophoneSound(7);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 255, 0, 136) // Change text color
-                    ),
-                child: Text('Play7')),
+            Expanded(
+                child: TextButton(
+              onPressed: () {
+                xylophoneSound(1);
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: const Color.fromARGB(249, 244, 0, 0),
+              ),
+              child: Text('Play', style: TextStyle(color: Colors.white)),
+            )),
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    xylophoneSound(2);
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(
+                          255, 255, 123, 0) // Change text color
+                      ),
+                  child: Text('Play 2', style: TextStyle(color: Colors.white))),
+            ),
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    xylophoneSound(3);
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(
+                          255, 255, 247, 0) // Change text color
+                      ),
+                  child: Text('Play 3', style: TextStyle(color: Colors.white))),
+            ),
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    xylophoneSound(4);
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(
+                          255, 11, 121, 28) // Change text color
+                      ),
+                  child: Text('Play4', style: TextStyle(color: Colors.white))),
+            ),
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    xylophoneSound(5);
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(
+                          255, 28, 92, 40) // Change text color
+                      ),
+                  child: Text('Play5', style: TextStyle(color: Colors.white))),
+            ),
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    xylophoneSound(6);
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(
+                          255, 34, 96, 172) // Change text color
+                      ),
+                  child: Text('Play6', style: TextStyle(color: Colors.white))),
+            ),
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    xylophoneSound(7);
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(
+                          255, 255, 0, 136) // Change text color
+                      ),
+                  child: Text('Play7', style: TextStyle(color: Colors.white))),
+            ),
           ],
         )),
       ),
